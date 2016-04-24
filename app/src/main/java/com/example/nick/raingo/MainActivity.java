c46f9749f55f9ac5d353e6c7cb18f71da5766d39
meansbusiness <meansbusiness24@gmail.com>
2016-04-24 16:49:40 -0400

    Changes and additions to schedule page

--- app/src/main/java/com/example/nick/raingo/MainActivity.java
+++ app/src/main/java/com/example/nick/raingo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.nick.raingo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
