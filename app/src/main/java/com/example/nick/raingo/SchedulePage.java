b0f1b177f62385df030620c0bf2bd136a34aebce
meansbusiness <meansbusiness24@gmail.com>
2016-04-24 03:47:39 -0400

    Added action bar and calendar to schedule page

--- app/src/main/java/com/example/nick/raingo/SchedulePage.java
+++ app/src/main/java/com/example/nick/raingo/SchedulePage.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 /**
  * Created by Nick on 4/8/2016.
@@ -13,12 +15,18 @@ public class SchedulePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.schedule_layout);
+        Toolbar scheduleToolBar = (Toolbar) findViewById(R.id.schedule_toolbar);
+        setSupportActionBar(scheduleToolBar);
 
         Intent activityThatCalled = getIntent();
 
         //this gets the activity that called this activity, the data that we passed
         String previousActivity = activityThatCalled.getExtras().getString("callingActivity");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_schedule, menu);
+        return true;
     }
 }
